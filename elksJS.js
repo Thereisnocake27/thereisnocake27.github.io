8bf8a79a466b14cd587a8754605b6d956e91e405
Thereisnocake27 <trey@roth-niggemann.com>
2025-10-14 16:50:23 -0700

    Update elksJS.js

--- elksJS.js
+++ elksJS.js
@@ -1,4 +1,5 @@
-            const topBarContent = `
+/*
+const topBarContent = `
 <li> <a href="javascript:;">Top-bar item</a> </li>
 <li class="drop-mobile"> <a href="javascript:;">Top-bar item</a> </li>
 <li> <a href="javascript:;">Top-bar item</a> </li>
@@ -43,6 +44,41 @@
 <div class="heading"> Icons </div>
 <div class="big-item"> <a href="javascript:;"></a> <a href="javascript:;"></a> <a href="javascript:;"></a> <a href="javascript:;"></a> </div>
 `;
+*/
+
+const topBarContent = `
+<li> <a href="javascript:;">Pay Dues</a> </li>
+<li class="drop-mobile"> <a href="javascript:;">Site Map</a> </li>
+<li> <a href="javascript:;">Calender</a> </li>
+<li class="drop-down">
+	<a href="javascript:;">More</a> 
+	<ul>
+		<li> <a href="javascript:;">Schedule Event</a> </li>
+		<li> <a href="javascript:;">Committees</a> </li>
+		<li> <a href="javascript:;">Become a Member</a> </li>
+		<li> <a href="javascript:;">Volunteer</a> </li>
+	</ul>
+</li>`;
+            const sideBarContent = `
+            <div id="side-bar-header">
+	<a href="/"></a> 
+	<div>
+		<h1><a href="/">Elks 142</a></h1>
+		<h2>Milwaukie-Portland</h2>
+	</div>
+</div>
+<div class="social-wrapper"> <a class="social-media" href="https://www.facebook.com/p/Milwaukie-Portland-Elks-142-100070042659972"> Facebook </a> </div>
+<div class="heading"> Quick Links </div>
+<ul>
+	<li> <a href="javascript:;">Menu</a> </li>
+	<li> <a href="javascript:;">Elks Pool</a> </li>
+	<li> <a href="javascript:;">Hollywood Nights</a> </li>
+	<li> <a href="javascript:;">Care and Share</a> </li>
+	<li> <a href="javascript:;">Elks Eye Care</a> </li>
+	<li> <a href="javascript:;">Americanism</a> </li>
+	<li> <a href="javascript:;">Officers</a> </li>
+</ul>
+`;
 
 function appendTheme(theme) {
 	var head = document.head;
@@ -83,5 +119,6 @@ function appendTheme(theme) {
                     closeMenu(closeMenuButton);
                 });
             });
+
 
 
